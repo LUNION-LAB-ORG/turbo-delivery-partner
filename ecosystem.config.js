d988c9fffced76572376d29d771892e0e1c3b7c5
AndersonKouadioDev <andersonkouadio0118@gmail.com>
2025-06-09 20:58:10 +0000

    add ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,11 @@ module.exports = {
 	apps: [{
 		name: 'partner-website',
 		script: 'pnpm',
-		args: 'start --port 3012',
-		cwd: '/var/www/turbo/test/turbo-delivery-partner',
+		args: 'start --port 3002',
+		cwd: '/var/www/turbo/prod/turbo-delivery-partner',
 		env: {
-			NODE_ENV: 'development',
-			PORT: 3012 // Port sur lequel l'application va tourner
+			NODE_ENV: 'production',
+			PORT: 3002 // Port sur lequel l'application va tourner
 		}
 	}]
 }
