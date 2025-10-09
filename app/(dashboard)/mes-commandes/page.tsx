5f274ba19c16b2daf0e1dbd2f326ffe5cd8e1dbc
Yamoussa Keita <Yamoussa.keita@lunion-lab.com>
2025-10-09 17:52:31 +0000

    modified:   app/(dashboard)/mes-commandes/page.tsx

--- app/(dashboard)/mes-commandes/page.tsx
+++ app/(dashboard)/mes-commandes/page.tsx
@@ -2,12 +2,20 @@ import { auth } from '@/auth';
 import Content from './content';
 import React, { Suspense } from 'react';
 import Loading from '@/components/layouts/loading';
+import { rechercherCommandesExterne } from '@/src/actions/commandes.actions';
 
 export default async function Page() {
     const session = await auth();
+
+    const commandes = await rechercherCommandesExterne({
+        restaurantId: session?.user?.restauranID,
+        page: 0,
+        size: 10,
+    });
+
     return (
         <Suspense fallback={<Loading />}>
-            <Content />
+            <Content commandesInitiales={commandes} session={session} />
         </Suspense>
     );
 }
