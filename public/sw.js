a285d97cdd2d249e5e3416202dff9acf0087a2fd
Yamoussa Keita <Yamoussa.keita@lunion-lab.com>
2025-11-06 18:31:50 +0000

    modified:   public/sw.js 	modified:   public/sw.js.map

--- public/sw.js
+++ public/sw.js
@@ -13,7 +13,7 @@
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
-  let registry = {};
+  let registry = {};  
 
   // Used for `eval` and `importScripts` where we can't get script URL by other means.
   // In both cases, it's safe to use a global var because those functions are synchronous.
